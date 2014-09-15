4559f6a21f406b401cb2151dced2d68b62c3cde4
joshuablackstone <joshuablackstone@hotmail.com>
2014-09-15 09:34:59 -0400

    Completed Version 1

--- Scripts/home.js
+++ Scripts/home.js
@@ -1,3 +1,17 @@
 ﻿$(function () {
     $('#currentYear').text(new Date().getFullYear());
+
+    $('.modal-photos img').css('cursor', 'pointer').click(function (e) {
+        var imgUrl = $(this).attr('src');
+        $('#home-photo').attr('src', imgUrl).addClass('img-thumbnail');
+        $('#btnDownload').data('src', imgUrl);
+        $('#myModal').modal('show');
+    });
+
+    $('#btnDownload').click(function () {
+        var src = $(this).data('src');
+        if (src == '')
+            return false;
+        window.open(src);
+    });
 });
