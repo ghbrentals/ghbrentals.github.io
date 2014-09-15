07bc7a9a8e9f09f481cfd0a3d95e1170aaffc612
joshuablackstone <joshuablackstone@hotmail.com>
2014-09-15 13:30:47 -0400

    Final first Round changes

--- Scripts/home.js
+++ Scripts/home.js
@@ -1,12 +1,19 @@
 ﻿$(function () {
     $('#currentYear').text(new Date().getFullYear());
 
-    $('.modal-photos img').css('cursor', 'pointer').click(function (e) {
-        var imgUrl = $(this).attr('src');
-        $('#home-photo').attr('src', imgUrl).addClass('img-thumbnail');
-        $('#btnDownload').data('src', imgUrl);
-        $('#myModal').modal('show');
-    });
+    $('.modal-photos img')
+        .css('cursor', 'pointer')
+        .hover(function () {
+            $(this).addClass('active');
+        }, function () {
+            $(this).removeClass('active');
+        })
+        .click(function (e) {
+            var imgUrl = $(this).attr('src');
+            $('#home-photo').attr('src', imgUrl).addClass('img-thumbnail');
+            $('#btnDownload').data('src', imgUrl);
+            $('#myModal').modal('show');
+        });
 
     $('#btnDownload').click(function () {
         var src = $(this).data('src');
---
+++ Scripts/phoneCheck.js
@@ -0,0 +1,18 @@
+﻿var phoneCheck = function () {
+    var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+    var mobile = width <= 500;
+    if (mobile) {
+        $('.phone').hide();
+        $('.mobilePhone').show();
+    }
+    else {
+        $('.mobilePhone').hide();
+        $('.phone').show();
+    }
+}
+$(function () {
+    phoneCheck();
+    $(window).resize(function () {
+        phoneCheck();
+    });
+});
